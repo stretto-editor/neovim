48319e110ccb9885fa8fb0feac0146736fbafb15
aym7 <aym.collange@gmail.com>
2016-06-01 17:43:25 +0200

    found a better way for deleting messages in live mode

--- src/nvim/ex_cmds.c
+++ src/nvim/ex_cmds.c
@@ -2919,6 +2919,7 @@ void do_sub(exarg_T *eap)
   static int do_ask = FALSE;            /* ask for confirmation */
   static bool do_count = false;         /* count only */
   //static int do_error = TRUE;           /* if false, ignore errors */
+  // if live mode, ignore errors
   static int do_error = FALSE;           /* if false, ignore errors */
   static int do_print = FALSE;          /* print last line with subs. */
   static int do_list = FALSE;           /* list last line with subs. */
@@ -3082,7 +3083,7 @@ void do_sub(exarg_T *eap)
     do_all = p_gd ? TRUE : FALSE;
 
     do_ask = FALSE;
-//    do_error = TRUE;
+    do_error = (EVENT_COLON == 1) ? FALSE : TRUE;
     do_print = FALSE;
     do_count = false;
     do_number = FALSE;
@@ -3815,8 +3816,10 @@ skip:
         else
           beginline(BL_WHITE | BL_FIX);
       }
-// TODO(aym7) find a better way for silent mode      if (!do_sub_msg(do_count) && do_ask)
-//        MSG("");
+      if(EVENT_COLON != 1) { // live_mode : no message in command line 
+        if (!do_sub_msg(do_count) && do_ask)
+          MSG("");
+      }
     } else
       global_need_beginline = TRUE;
     if (do_print)
@@ -6062,7 +6065,6 @@ void do_live_sub(exarg_T *eap) {
   }
   char_u *arg;
   char_u *tmp;
-  p_lz = 1;
   switch (cmdl_progress) {
     case LS_NO_WD: 
       break;
